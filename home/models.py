3640bba885dc909170d236f56292d25dcd644d11
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-05-24 13:09:58 -0400

    improve

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class MyModel(models.Model):
+    # Fields
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
