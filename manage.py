140d2c16a105ce07541c973fa6b2c0be9bc2dea8
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-05-24 13:10:03 -0400

    improve

--- manage.py
+++ manage.py
@@ -2,21 +2,23 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from typing import List, Tuple
 
-
-def main():
+def main() -> None:
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        error_msg = (
+            "Couldn't import Django. Make sure it's installed and available "
+            "on your PYTHONPATH environment variable. If you're using a "
+            "virtual environment, did you remember to activate it?"
+        )
+        raise ImportError(error_msg) from exc
     execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
     main()
+
