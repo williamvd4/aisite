7c533bdbf207e8af99a4a6dea1989014ecf396d8
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-05-24 13:10:02 -0400

    improve

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,30 @@ from django.urls import path
 
 from . import views
 
+app_name = 'your_app_name'  # replace with your app name
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
+
+# in views.py
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+class LogoutView(TemplateView):
+    template_name = 'logout.html'
