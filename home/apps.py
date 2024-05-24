a165ea1c6550fcd9a913415a32e92a8eb252a574
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-05-24 13:09:57 -0400

    improve

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
-
 class HomeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "home"
+    name = "app_name"  # replace 'app_name' with the actual name of the app
+
+    def ready(self):
+        # import any models or perform any other setup here
+        from . import signals  # replace 'signals' with the actual name of the signals module, if any
+
