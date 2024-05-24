f961c5ab3396fd4ba5162c796d69647b249ab96b
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-05-24 13:10:04 -0400

    improve

--- build.sh
+++ build.sh
@@ -1,10 +1,43 @@
 #!/usr/bin/env bash
+
 # exit on error
 set -o errexit
 
-python -m pip install --upgrade pip
+# pipefail ensures that a pipeline will produce a failure return code
+# if any command errors.
+set -o pipefail
 
-pip install -r requirements.txt
+# print commands before executing them
+set -o xtrace
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+# install dependencies
+if [[ ! -x "$(command -v curl)" ]]; then
+  echo 'Error: curl is not installed.' >&2
+  exit 1
+fi
+
+if ! curl -fsS https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -; then
+  echo 'Error: Failed to add Docker GPG key.' >&2
+  exit 1
+fi
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+# run as non-root user
+if ! id -u django >/dev/null 2>&1; then
+  sudo useradd -m -s /bin/bash django
+fi
+
+if ! groups django | grep -qw docker; then
+  sudo usermod -aG docker django
+fi
+
+sudo chown django:django /var/run/docker.sock
+
+su -c "pip install --upgrade pip" django
+su -c "pip install -r requirements.txt" django
+
+su -c "python manage.py collectstatic --no-input" django
+su -c "python manage.py migrate" django
