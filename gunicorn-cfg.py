ad7d6c4b6e6bc40a69120a27363f6fdd1add8c61
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-05-30 09:31:09 -0400

    improve

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,11 +1,3 @@
 # -*- encoding: utf-8 -*-
 """
-Copyright (c) 2019 - present AppSeed.us
-"""
-
-bind = '127.0.0.1:5005' # Use the loopback address instead of 0.0.0.0 for better security
-workers = 2 # Increase the number of workers to improve performance
-accesslog = '-'
-loglevel = 'info' # Set log level to info for less verbose output
-capture_output = False # Disable output capture for better error reporting
-# disable_stdio_inheritance = True # Uncomment this line if you're using Gunicorn with a systemd systemd service
+Copyright (c) 2
