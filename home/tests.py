8480879f8cbc6bd4d470a945ee6ca5d88570e8cc
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-05-30 09:31:18 -0400

    improve

--- home/tests.py
+++ home/tests.py
@@ -1,12 +1,19 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 from myapp.views import HomePageView
 
 class HomePageViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
     def test_home_page_status_code(self):
-        response = self.client.get('/')
+        request = self.factory.get('/')
+        view = HomePageView.as_view()
+        response = view(request)
         self.assertEquals(response.status_code, 200)
 
     def test_home_page_content(self):
-        response = self.client.get('/')
+        request = self.factory.get('/')
+        view = HomePageView.as_view()
+        response = view(request)
         self.assertContains(response, "Welcome to my website!")
 
