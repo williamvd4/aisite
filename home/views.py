614120e7ffdd4df702e704c5f0122d747c8b60ec
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-05-30 09:31:18 -0400

    improve

--- home/views.py
+++ home/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+# yourapp/views.py
+
+from django.shortcuts import render
 from django.http import HttpResponse
 
+# Name of the app
 # Create your views here.
 
-def homepage(request):
+def home(request):
     """Homepage view"""
-    # Page from the theme 
     context = {'message': 'Welcome to our website!'}
     return render(request, 'pages/index.html', context)
 
 def page_not_found(request, exception):
     """404 error handler view"""
     return render(request, 'errors/404.html', status=404)
-
 
 handler404 = 'yourapp.views.page_not_found'
