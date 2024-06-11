575759f997af3c756ddcf64efd88ce355979a5e3
williamvd4 <153226281+williamvd4@users.noreply.github.com>
2024-06-11 12:59:44 -0400

    improve

--- home/models.py
+++ home/models.py
@@ -1,23 +1,3 @@
 from django.db import models
+from django.utils.timezone import now
 
-class MyModel(models.Model):
-    """
-    A model representing some kind of object with a name, description,
-    creation time, and last modification time.
-    """
-    # Fields
-    name = models.CharField(
-        max_length=200,
-        help_text='The name of the object.'
-    )
-    description = models.TextField(
-        help_text='A detailed description of the object.'
-    )
-    creation_time = models.DateTimeField(
-        auto_now_add=True,
-        help_text='The time when the object was first created.'
-    )
-    last_modified_time = models.DateTimeField(
-        auto_now=True,
-        help_text='The time when the object was last modified.'
-    )
